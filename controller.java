9a67a96e31a29cc65af6ee987b22d5c6f7d3cfa4
voeunlyna-cyber <voeunlyna@gmail.com>
2026-06-12 14:07:23 +0700

    11

---
+++ controller.java
@@ -0,0 +1,32 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+
+public class controller {
+
+    @FXML
+    private Button button_submit;
+
+    @FXML
+    private ChoiceBox<?> choicebox_1;
+
+    @FXML
+    private ChoiceBox<?> choisebox_2;
+
+    @FXML
+    private ChoiceBox<?> choisebox_3;
+
+    @FXML
+    private Label label_clock;
+
+    @FXML
+    private Label label_result;
+
+    @FXML
+    void on_submit(ActionEvent event) {
+
+    }
+
+}
