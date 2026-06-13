8a39c1fe70b5b65ea4f3e0850f6c2abdbccface8
voeunlyna-cyber <voeunlyna@gmail.com>
2026-06-13 09:44:19 +0700

    kk

--- Appliction.java
+++
@@ -1,9 +0,0 @@
-// public class Appliction {
-
-//     public void start(Stage stage) throws Exception {
-//         // TODO Auto-generated method stub
-//         throw new UnsupportedOperationException("Unimplemented method 'start'");
-//     }
-
-
-// }
